012697ccfe6fb23d1b5cddfc70ad71bd3a8ffc56
fortunehu <33173490+fortunehu@users.noreply.github.com>
2018-05-02 16:53:44 +0800

    * weixin cloude;

--- weixinwordcloud.py
+++ weixinwordcloud.py
@@ -1,36 +1,48 @@
-#微信发消息
-from __future__ import unicode_literals
-from threading import Timer
-from wxpy import *
-import requests
+# -*- coding: utf-8 -*-
+import itchat
+import re
+import os
+# wordcloud词云
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, ImageColorGenerator
+import PIL.Image as Image
+import numpy as np
 
-bot = Bot()
-#bot = Bot(console_qr=2, cache_path="botoo.pkl")
+itchat.login()
+friends = itchat.get_friends(update=True)[0:]
+tList = []
+for i in friends:
+    signature = i["Signature"].replace(" ", "").replace("span", "").replace("class", "").replace("emoji", "")
+    rep = re.compile("1f\d.+")
+    signature = rep.sub("", signature)
+    tList.append(signature)
 
-def get_news1():
-     # 获取金山词霸每日一句，英文和翻译
-    url = "http://open.iciba.com/dsapi/"
-    r = requests.get(url)
-    contents = r.json()['content']
-    translation = r.json()['translation']
-    return contents, translation
+# 拼接字符串
+text = "".join(tList)
+#text = "The MultiTaskLasso is a linear model that estimates sparse coefficients for multiple regression problems jointly: y is a 2D array, of shape (n_samples, n_tasks). The constraint is that the selected features are the same for all the regression problems, also called tasks.The following figure compares the location of the non-zeros in W obtained with a simple Lasso or a MultiTaskLasso. The Lasso estimates yields scattered non-zeros while the non-zeros of the MultiTaskLasso are full columns"
+# jieba分词
+import jieba
+wordlist_jieba = jieba.cut(text, cut_all=True)
+wl_space_split = " ".join(wordlist_jieba)
 
-def send_news():
-    try:
-        #my_friends = bot.friends();
-        my_friend = bot.friends().search(u'GreatWall')[0] #备注名
-        my_friend.send(get_news1()[0])
-        my_friend.send(get_news1()[1][5:])
-        my_friend.send(u"来自ciba的心灵鸡汤！")
-        print("send ok")
-        #t = Timer(30,send_news)
-        #t.start()
-    except:
-        #my_friends = bot.friends();
-        print("send error")
-        my_friend = bot.friends().search(u'GreatWall')[0] #备注名
-        my_friend.send(u"今天消息发送失败了")
+print(wl_space_split)
+
+d = os.path.dirname(__file__)
+alice_coloring = np.array(Image.open(os.path.join(d, "wechat.jpg")))
+
+# 这里要选择字体存放路径，这里是Mac的，win的字体在windows／Fonts中
+my_wordcloud = WordCloud(background_color="white", max_words=2000, mask=alice_coloring,
+                         max_font_size=300, random_state=42,
+                         font_path='C:/Windows/Fonts/Arial/simsun.ttc').generate(wl_space_split)
 
 
-if __name__ == "__main__":
-    send_news()
+image_colors = ImageColorGenerator(alice_coloring)
+
+my_wordcloud.to_file(os.path.join(d, "wechat_cloud.png"))
+itchat.send_image(os.path.join(d, "wechat_cloud.png"), 'filehelper')
+
+plt.imshow(my_wordcloud.recolor(color_func=image_colors))
+plt.imshow(my_wordcloud)
+plt.axis("off")
+plt.show()
+
