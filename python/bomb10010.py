b5f5be0c8c344d36672359eb96fa54119d0da330
fortunehu <33173490+fortunehu@users.noreply.github.com>
2018-06-29 19:30:45 +0800

    + dianxinbomb;

---
+++ python/bomb10010.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+# from selenium import WebElement
+import time
+
+#10010 bomb
+driver = webdriver.Chrome()
+driver.get('https://uac.10010.com/cust/userinfo/userInfoInit')
+
+#<iframe id="login_iframe" frameborder="0" scrolling="no" style="width: 438px;height: 496px; border: 0;" marginheight="0" marginwidth="0" allowtransparency="true" src="https://uac.10010.com/portal/custLogin"></iframe>
+driver.switch_to_frame("login_iframe")
+
+#<a href="javascript:void(0);" class="randomCodeLogin" style="color:#ff6000" id="randomPwdTips">
+time.sleep(3)
+driver.find_element_by_id("randomPwdTips").click()
+time.sleep(1)
+driver.find_element_by_id("userName").send_keys("176*****")
+time.sleep(1)
+driver.find_element_by_id("randomCode").click()
+time.sleep(3)
+driver.quit()
